42e135e0bf5a496efec5edb8e9292a80eb0112ae
eelbe029 <146129886+eelbe029@users.noreply.github.com>
2024-05-29 13:47:53 +0000

    Finished problem 347

--- 347.TopKFrequentElements.py
+++ 347.TopKFrequentElements.py
@@ -14,13 +14,14 @@ def topKFrequent(nums, k):
     print(liste) 
     for num,freq in liste.items():
         if len(top_el) < k:
-            top_el.append(num)
+            top_el.append([num,freq])
         else:
             old = top_el.pop()
-            if liste[old] < freq:
-                top_el.append(num)
+            if old[1] < freq:
+                top_el.append([num,freq])
             else:
                 top_el.append(old)
-        top_el = sorted(top_el,reverse=True)
-    return top_el[:k]
+        top_el.sort(key=lambda x: x[1], reverse=True)
+    return [top_el[i][0] for i in range(k)]
+
 print(topKFrequent([6,0,1,4,9,7,-3,1,-4,-8,4,-7,-3,3,2,-3,9,5,-4,0],6))
